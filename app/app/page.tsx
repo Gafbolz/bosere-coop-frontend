a2fabf12d6eac4434952ff11638b70d5604bea33
Gafbolz <34697280+Gafbolz@users.noreply.github.com>
2026-03-25 22:34:05 +0100

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Bosere Cooperative</h1>
+      <p>Go to dashboard:</p>
+      <a href="/dashboard">Open Dashboard</a>
+    </main>
+  );
+}
